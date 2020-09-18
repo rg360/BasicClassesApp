cd10186a69d5f82ecaf5730c4d294d5611c5981e
rg360 <rg360@ymail.com>
2020-09-17 20:07:38 -0500

    Basic Classes MyCar first commit

---
+++ BasicClasses/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace BasicClasses
+{
+    public class Car
+    {
+        public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public int CarYear { get; set; }
+
+        public Car()
+        {
+
+        }
+    }
+}
---
+++ BasicClasses/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+
+            Car myCar = new Car();
+
+            myCar.CarMake = "Jeep";
+            myCar.CarModel = "Wrangler";
+            myCar.CarYear = 2019;
+
+            Console.WriteLine(myCar.CarYear + " " + myCar.CarMake + " " + myCar.CarModel);
+
+
+        }
+    }
+}
